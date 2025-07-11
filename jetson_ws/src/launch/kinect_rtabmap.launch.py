a7bd024dd323f987130b7963eb714e8501ccbd6b
Breno <breno.henrique.silva@ee.ufcg.edu.br>
2025-07-11 16:26:11 -0300

    update kinect launch file

--- jetson_ws/src/launch/kinect_rtabmap.launch.py
+++ jetson_ws/src/launch/kinect_rtabmap.launch.py
@@ -1,26 +1,14 @@
-# Conteúdo para iniciar_kinect.launch.py
-
-import os
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
+from launch_ros.actions import Node
 
-# Esta é a função principal que o ros2 launch procura
+
 def generate_launch_description():
-
-    # Encontra o caminho para o pacote do kinect que compilamos
-    kinect_package_dir = get_package_share_directory('kinect_ros2')
-
-    # Cria a ação de incluir o launch file do kinect
-    # Estamos reutilizando o launch file que já vem com o driver
-    start_kinect_cmd = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(kinect_package_dir, 'launch', 'showimage.launch.py')
-        )
-    )
-    
-    # Retorna a descrição do lançamento, contendo apenas a ação de iniciar o kinect
     return LaunchDescription([
-        start_kinect_cmd
-    ])
+        Node(
+            package='kinect_ros2',
+            namespace='kinect',
+            executable='kinect_ros2_node',
+            name='kinect_ros2'
+        ),
+    ]
+)
