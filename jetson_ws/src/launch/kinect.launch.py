325dfcbe894435c9fe919fd5802ac4b8d04ec4ca
breno-martins <breno.henrique.silva@ee.ufcg.edu.br>
2025-07-14 11:51:08 -0300

    update code in Kinect_launch file and back do old code in docker-compose-yml

--- jetson_ws/src/launch/kinect.launch.py
+++ jetson_ws/src/launch/kinect.launch.py
@@ -4,11 +4,9 @@ from launch_ros.actions import Node
 def generate_launch_description():
     
     # Define o caminho direto para o arquivo de configuração do RViz DENTRO do container.
-    # Sabemos que ele estará aqui por causa do nosso mapeamento no docker-compose.yml.
-    rviz_config_file = '/root/ros_ws/config/kinect_config.rviz'
+    rviz_config_file = '/root/ros_ws/src/config/kinect_config.rviz'
 
     # Define o nó para a câmera Kinect.
-    # Vamos usar o launch file que vem com o pacote, pois ele já é bem configurado.
     kinect_node = Node(
         package='kinect_ros2',
         executable='kinect_ros2_node',
